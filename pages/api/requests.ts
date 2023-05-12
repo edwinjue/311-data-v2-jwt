39c8fe7662ddced927e9aaee7a6f0662bea76b6a
Edwin J <hungrylulu8@gmail.com>
2023-05-12 00:27:44 -0700

    initial working pages/api/requests.ts

--- pages/api/requests.ts
+++ pages/api/requests.ts
@@ -1,20 +1,18 @@
-import { NextApiRequest } from 'next'
+import { type NextRequest } from 'next/server'
 import { jsonResponse } from '@lib/utils'
-import axios from 'axios'
+import querystring from 'querystring'
 
-export const config = {
-  runtime: 'edge',
-}
-
-// We make API requests on a per-day basis. On average, there are about 4k
-// requests per day, so 10k is a large safety margin.
 const REQUEST_LIMIT = 10000;
 
-export default async function requests(req: NextApiRequest) {
-  try{
+export const config = {
+  runtime: 'edge'
+}
 
-    let startDate = req.query?.startDate || null;
-    let endDate = req.query?.endDate || null;
+export default async function requests(req: NextRequest) {
+  try{
+    const query = querystring.parse(req.url.split('?')[1])
+    const startDate = query.startDate
+    const endDate = query.endDate
 
     if(!!startDate === false || !!endDate === false){
       return jsonResponse(400, { error: { message: 'Missing required query parameter. startDate and endDate expected.' } })
@@ -22,12 +20,15 @@ export default async function requests(req: NextApiRequest) {
 
     const url = `${process.env.SOCRATA_API_URL}?$where=` + 
                 encodeURI(`createddate between '${startDate}' and '${endDate}'`) +
-                `&$limit=${REQUEST_LIMIT}&$app_token=${process.env.SOCRATA_TOKEN}`
+                `&$limit=${REQUEST_LIMIT}`
  
-    const data = await axios.get(url)
-    
-    console.log('/api/requests: returning ',{data})
- 
+    const headers = new Headers({
+      'X-App-Token': process.env.SOCRATA_TOKEN || ''
+    })
+
+    const response = await fetch(url, { headers })
+    const data = await response.json()
+
     return jsonResponse(200, { success: true, data })
   }
   catch(e){
